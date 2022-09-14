3383082cce7ce45852e6e51bbaa7b63cea890c7f
Craig Nelson <34287157+supahcraig@users.noreply.github.com>
2022-09-13 20:47:24 -0500

    Create manipulateFlowFileCSV.py

---
+++ Labs/manipulateFlowFileCSV.py
@@ -0,0 +1,24 @@
+import json
+import java.io
+from datetime import datetime
+from org.apache.commons.io import IOUtils
+from java.nio.charset import StandardCharsets
+from org.apache.nifi.processor.io import StreamCallback
+
+class PyStreamCallback(StreamCallback):
+	def __init__(self):
+		pass
+
+	def process(self, inputStream, outputStream):
+		text = IOUtils.toString(inputStream, StandardCharsets.UTF_8)
+		today_string = datetime.today().strftime('%Y-%m-%d')
+		text = text + ', ' + today_string
+
+		outputStream.write(bytearray(text.encode('utf-8')))
+
+
+flowFile = session.get()
+if (flowFile != None):
+	flowFile = session.write(flowFile,PyStreamCallback())
+	session.transfer(flowFile, REL_SUCCESS)
+	
